b8274c4725663caa631a77f094a61ccb20c66878
leoschaurichdev <leoschaurich7@gmail.com>
2024-11-29 21:16:19 -0300

    Add project files.

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+            var result = await _paymentService.Process(paymentInfoInputModel);
+
+            if (!result)
+            {
+                return BadRequest(result); 
+            }
+
+            return NoContent();
+        }
+
+    }
+}
